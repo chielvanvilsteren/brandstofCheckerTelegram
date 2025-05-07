ba5ca19f3612eb2b96f04f1c4d8f9f69033ca4da
chielvanvilsteren <chielvanvilsteren@gmail.com>
2025-05-07 20:38:40 +0200

    using port 80

--- logger/server.js
+++ logger/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 const LOG_PATH = path.join(__dirname, "../logs/weather.log");
 const ERROR_LOG_PATH = path.join(__dirname, "../logs/error.log");
